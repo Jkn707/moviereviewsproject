0a8034c6e6e8600fe38c51b2b580e1716887d744
Jk <juanandrs.montoyagaleano@gmail.com>
2024-02-08 13:47:01 -0500

    Models.py corregido

--- movie/models.py
+++ movie/models.py
@@ -4,6 +4,6 @@ from django.db import models
 
 class Movie(models.Model):
     title = models.CharField(max_length=100)
-    description = models.TextField()
+    description = models.CharField(max_length=250)
     image = models.ImageField(upload_to='movie/images/')
     url = models.URLField(blank=True)
