53f119afc053157a1deb014e3652400cea6c0f95
Jk <juanandrs.montoyagaleano@gmail.com>
2024-02-08 14:04:36 -0500

    Fixed error in the settings.py (Reverted BASE_DIR to the original value)

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -14,12 +14,10 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
